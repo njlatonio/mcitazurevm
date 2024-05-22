222f048bebe22fbd9d14f7731dd2c0b5cb18868d
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-22 19:56:07 -0400

    Update virtualmachine.tf

--- virtualmachine.tf
+++ virtualmachine.tf
@@ -34,8 +34,8 @@ resource "azurerm_resource_group" "example" {
 resource "azurerm_virtual_network" "main" {
   name                = "network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.[each.value.location]
-  resource_group_name = azurerm_resource_group.example.[each.value.name]
+  location            = azurerm_resource_group.example[each.value.location]
+  resource_group_name = azurerm_resource_group.example[each.value.name]
 }
 
 resource "azurerm_subnet" "internal" {
