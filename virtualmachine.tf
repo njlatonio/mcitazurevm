84287cac9bc6d234022ecfcf7d18e3794b768b9e
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-22 20:51:26 -0400

    Update virtualmachine.tf

--- virtualmachine.tf
+++ virtualmachine.tf
@@ -1,3 +1,4 @@
+/*
 locals{
   #Question 11
   vm_names=["firstvm","secondvm","thirdvm","fourthvm","fifthvm"]
@@ -12,24 +13,13 @@ locals{
     ]
 ])
 
-  #Question 13
-  azurevmconfig_Q13=yamldecode(file("${path.module}/vmfolder_Q13/azurevmresourcename.yaml"))
-  azurevmlist_Q13 = [
-   for azurevm in local.azurevmconfig_Q13.rgconfig : {
-      name=azurevm.name
-      location=azurevm.location
-      }
-  ]
-
 }
 
 
 resource "azurerm_resource_group" "example" {
-  for_each = {for value in local.azurevmlist_Q13: value.name=>value}
-  name     = each.value.name
-  location = each.value.location
+  name     = "resources"
+  location = "West Europe"
 }
-
 
 resource "azurerm_virtual_network" "main" {
   name                = "network"
@@ -57,11 +47,7 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-/*
-resource "azurerm_resource_group" "example" {
-  name     = "resources"
-  location = "West Europe"
-}
+
 #Question 11
 resource "azurerm_virtual_machine" "main" {
   for_each              = {for machine in local.vm_names: machine=>machine}
