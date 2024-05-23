0710b304fa8b005b45ce44fef64d3023b6d2830c
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-22 21:09:48 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -16,3 +16,66 @@ variable "admin_username"{
 variable "admin_password"{
   type=string
 }
+variable "name"{
+  type=string
+} 
+variable "location"{
+  type=string
+} 
+variable "virtual_network_name"{
+  type=string
+} 
+variable "address_space"{
+  type=list(string)
+}
+variable "subnet_name"{
+  type=string
+} 
+variable "address_prefixes"{
+  type=list(string)
+}
+variable "nic_name"{
+  type=string
+} 
+variable "ip_config_name"{
+  type=string
+}
+variable "private_ip_address_allocation"{
+  type=string
+}
+variable "vm_size"{
+  type=string
+}
+variable "publisher"{
+  type=string
+}
+variable "offer"{
+  type=string
+}
+variable "sku"{
+  type=string
+}
+variable "version"{
+  type=string
+}
+variable "storage_os_disk_name"{
+  type=string
+}
+variable "caching"{
+  type=string
+}
+variable "create_option"{
+  type=string
+}
+variable "managed_disk_type"{
+  type=string
+}
+variable "computer_name"{
+  type=string
+}
+variable "disable_password_authentication"{
+  type=bool
+}
+variable "environment"{
+  type=string
+}
