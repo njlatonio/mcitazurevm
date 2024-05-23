b7c063116e9a1ab70faf5ca9d99221d0c75891a7
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-22 20:53:48 -0400

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,17 +1,19 @@
 locals{
   #Question 13
   azurevmconfig_Q13=[for f in fileset("${path.module}/vmfolder_Q13", "[^_]*.yaml") : yamldecode(file("${path.module}/vmfolder_Q13/${f}"))]
-  azurevmlist_Q13 = [
-   for azurevm in local.azurevmconfig_Q13.rgconfig : {
-      name=azurevm.name
-      location=azurevm.location
+  azurevmlist_Q13 = flatten([
+    for app in local.azurevmconfig_Q13 : [
+      for azurevm in try(app.rgconfig, []) :{
+        name=azurevm.name
+        location=azurevm.location
       }
-  ]
+    ]
+])
 
 }
 
 resource "azurerm_resource_group" "example" {
-  for_each = {for value in local.azurevmlist_Q13: value.name=>value}
+  for_each = {for value in local.azurevmlist_Q13: "${value.name}"=>value}
   name     = each.value.name
   location = each.value.location
 }
