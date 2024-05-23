c47dc69dc19a409688c4f3d515fec6d65ec2d011
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-22 21:09:53 -0400

    Update virtualmachine.tf

--- virtualmachine.tf
+++ virtualmachine.tf
@@ -16,33 +16,33 @@ locals{
 
 
 resource "azurerm_resource_group" "example" {
-  name     = "resources"
-  location = "West Europe"
+  name     = var.name
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "network"
-  address_space       = ["10.0.0.0/16"]
+  name                = var.virtual_network_name
+  address_space       = var.address_space
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+  name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefixes     = var.address_prefixes
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "nic"
+  name                = variable.nic_name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "testconfiguration1"
+    name                          = var.ip_config_name
     subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = var.private_ip_address_allocation
   }
 }
 
@@ -50,40 +50,34 @@ resource "azurerm_network_interface" "main" {
 #Question 11
 resource "azurerm_virtual_machine" "main" {
   for_each              = {for machine in local.vm_names: machine=>machine}
-  name                  = "vm"
+  name                  = each.value.name
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  vm_size               = var.vm_size
 
   storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.version
   }
   storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+    name              = var.storage_os_disk_name
+    caching           = var.caching
+    create_option     = var.create_option
+    managed_disk_type = var.managed_disk_type
   }
   os_profile {
-    computer_name  = "hostname"
+    computer_name  = var.computer_name
     admin_username = var.admin_username
     admin_password = var.admin_password
   }
   os_profile_linux_config {
-    disable_password_authentication = false
+    disable_password_authentication = var.disable_password_authentication
   }
   tags = {
-    environment = "staging"
+    environment = var.environment
   }
 }
 
@@ -95,7 +89,7 @@ resource "azurerm_virtual_machine" "main_yaml" {
   location              = azurerm_resource_group.example.location
   resource_group_name   = azurerm_resource_group.example.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = var.vm_size
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
   # delete_os_disk_on_termination = true
@@ -104,27 +98,27 @@ resource "azurerm_virtual_machine" "main_yaml" {
   # delete_data_disks_on_termination = true
 
   storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
+    publisher = var.publisher
+    offer     = var.offer
+    sku       = var.sku
+    version   = var.version
   }
   storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+    name              = var.storage_os_disk_name
+    caching           = var.caching
+    create_option     = var.create_option
+    managed_disk_type = var.managed_disk_type
   }
   os_profile {
-    computer_name  = "hostname"
+    computer_name  = var.computer_name
     admin_username = var.admin_username
     admin_password = var.admin_password
   }
   os_profile_linux_config {
-    disable_password_authentication = false
+    disable_password_authentication = var.disable_password_authentication
   }
   tags = {
-    environment = "staging"
+    environment = var.environment
   }
 }
 
