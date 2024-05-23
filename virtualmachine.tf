5499b071332faf8c78d562f478fed52e6d581518
njlatonio <168039404+njlatonio@users.noreply.github.com>
2024-05-22 20:18:03 -0400

    Update virtualmachine.tf

--- virtualmachine.tf
+++ virtualmachine.tf
@@ -29,37 +29,35 @@ resource "azurerm_resource_group" "example" {
   for_each = {for value in local.azurevmlist_Q13: "${value.name}"=>value}
   name     = each.value.name
   location = each.value.location
-}
 
 resource "azurerm_virtual_network" "main" {
-  for_each = {for value in local.azurevmlist_Q13: "${value.name}"=>value}
   name                = "network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example[each.value.location]
-  resource_group_name = azurerm_resource_group.example[each.value.name]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "internal" {
   for_each = {for value in local.azurevmlist_Q13: "${value.name}"=>value}
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example[each.value.name]
-  virtual_network_name = azurerm_virtual_network.main[each.value.name]
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
   for_each = {for value in local.azurevmlist_Q13: "${value.name}"=>value}
   name                = "nic"
-  location            = azurerm_resource_group.example[each.value.location]
-  resource_group_name = azurerm_resource_group.example[each.value.name]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal[each.value.id]
+    subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
 }
-
+}
 /*
 resource "azurerm_resource_group" "example" {
   name     = "resources"
